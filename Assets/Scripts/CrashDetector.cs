b7bc82835273f290fcaa7434289d7fbbdd1f789c
Gemma Campbell <gem.cam89@outlook.com>
2024-10-12 20:02:12 +0100

    Added: Scene Management - Invoke() to reload scene - Snow and finish line particle effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float crashDelay = 2f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
-            Debug.Log("Ouch!");
+            Invoke("ReloadScene", crashDelay);
         }
 
     }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+
+    [SerializeField] float finishDelay;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            Debug.Log("You finished!");
+            Invoke("ReloadScene", finishDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
